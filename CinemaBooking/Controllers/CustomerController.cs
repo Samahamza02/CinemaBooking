dbd02d4aa542cd42cd235d0b351c0af497d96ec0
Samahamza02 <samahamza51@gmail.com>
2026-08-16 00:29:20 +0300

    Update Cinema Booking project

---
+++ CinemaBooking/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using CinemaBooking.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaBooking.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // Customer Home
+        public IActionResult Index()
+        {
+            var movies = _context.Movies
+                .Include(m => m.Category)
+                .Include(m => m.Cinema)
+                .Where(m => m.Status == "Available")
+                .Take(6)
+                .ToList();
+
+            return View(movies);
+        }
+
+
+        // All Movies
+        public IActionResult Movies()
+        {
+            var movies = _context.Movies
+                .Include(m => m.Category)
+                .Include(m => m.Cinema)
+                .ToList();
+
+            return View(movies);
+        }
+    }
+}
